78a04a39fbbe65e20e9e36dd7aa7ae1a9fcf69f8
rishikesh-parspec <135440279+rishikesh-parspec@users.noreply.github.com>
2025-02-10 11:30:57 +0530

    Update memory.go

--- adapter/memory/memory.go
+++ adapter/memory/memory.go
@@ -30,7 +30,7 @@ import (
 	"sync"
 	"time"
 
-	cache "github.com/SporkHubr/echo-http-cache"
+	cache "github.com/rishikesh-parspec/echo-http-cache"
 )
 
 // Algorithm is the string type for caching algorithms labels.
